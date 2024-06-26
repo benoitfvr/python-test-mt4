124b0d1351135457c27caea184dcec7f80368049
Benoît Favrie <benoit14.favrie@gmail.com>
2024-06-26 15:05:48 +0200

    fix: rename all tests

--- test/test_main.py
+++ test/test_main.py
@@ -1,22 +1,22 @@
 import pytest
 from main import calculate, main
 
-def test_calculate_addition():
+def test_should_calculate_addition():
     assert calculate("20 5 +") == 25
 
-def test_calculate_subtraction():
+def test_should_calculate_subtraction():
     assert calculate("4 2 + 3 -") == 3
 
-def test_calculate_multiplication():
+def test_should_calculate_multiplication():
     assert calculate("3 5 8 * 7 + *") == 141
 
-def test_calculate_complex_expression():
+def test_should_calculate_complex_expression():
     assert calculate("3 5 8 * 7 + *") == 141
     
-def test_calculate_invalid_expression():
+def test_should_raise_error_invalid_expression():
     with pytest.raises(IndexError):
         calculate("4 +")
 
-def test_calculate_non_integer():
+def test_should_raise_error_non_integer():
     with pytest.raises(ValueError):
         calculate("4 a +")
